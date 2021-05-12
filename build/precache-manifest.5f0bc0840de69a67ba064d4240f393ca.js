552489d2bd34696d975530af0e710a20d228b3a8
Shuo Man <shuo.man@ge.com>
2021-05-12 15:49:40 +0800

    add build for page

---
+++ build/precache-manifest.5f0bc0840de69a67ba064d4240f393ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261f8fe2f65ff1636cab6c99ba4b6022",
+    "url": "/gym/index.html"
+  },
+  {
+    "revision": "850be63d5bb0b7593507",
+    "url": "/gym/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "09c3aba50ab1f31e8292",
+    "url": "/gym/static/css/main.7d20663e.chunk.css"
+  },
+  {
+    "revision": "850be63d5bb0b7593507",
+    "url": "/gym/static/js/2.b77817ab.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gym/static/js/2.b77817ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c3aba50ab1f31e8292",
+    "url": "/gym/static/js/main.60817914.chunk.js"
+  },
+  {
+    "revision": "6cd1126d2b0d3d9b2651",
+    "url": "/gym/static/js/runtime-main.2d4c3508.js"
+  }
+]);
